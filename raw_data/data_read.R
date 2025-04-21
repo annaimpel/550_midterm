ddd7938c79a077db7808679863218ca83f61a1fb
annaimpel <aimpell@emory.edu>
2025-04-21 14:28:22 -0400

    Update data_read.R

--- raw_data/data_read.R
+++ raw_data/data_read.R
@@ -1,2 +1,11 @@
+library(here)
+library(readr)
+
+# Define the download URL and destination path
 url <- "https://data.cdc.gov/api/views/2ew6-ywp6/rows.csv?accessType=DOWNLOAD"
-wastewater <- read_csv(url)
+destfile <- here("data", "wastewater.csv")
+
+# Only download if the file doesn't already exist
+if (!file.exists(destfile)) {
+  download.file(url, destfile, mode = "wb")
+}
